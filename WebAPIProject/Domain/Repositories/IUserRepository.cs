e29f5a76f466f01ea0ce3604bf4abd171713de98
Umut BÜYÜKYILDIZ <umut.buyukyildiz@tezmaksan.com>
2023-01-16 14:49:21 +0300

    Adding IUserRepository interface

---
+++ WebAPIProject/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIProject.Domain.Repositories
+{
+    interface IUserRepository
+    {
+        Task AddUser(User user);
+
+        User FindById(int userId);
+
+        User FindByEmailandPassword(string email, string password);
+
+        void SaveRefreshToken(int userId, string refreshToken);
+
+        User GetUserWithRefreshToken(string RefreshToken);
+
+        void RemoveRefreshToken(User user);
+
+
+
+
+    }
+}
